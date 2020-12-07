f7d47feccdcb625f7e257bae054d47c10716d451
Christina Cook <cmc.cook7@gmail.com>
2020-12-07 14:36:12 -0700

    Set up UserRepository test

---
+++ test/UserRepository-test.js
@@ -0,0 +1,22 @@
+const chai = require('chai');
+const expect = chai.expect;
+const UserRepository = require('../src/UserRepository');
+
+describe('UserRepository', () => {
+  it.skip('Should have users', () => {
+
+  });
+
+  it.skip('Should instantiate a new User', () => {
+
+  });
+
+  it.skip('Should be able to get a user by id', () => {
+
+  });
+
+  it.skip('Should calculate the average step goal', () => {
+
+  });
+
+});
