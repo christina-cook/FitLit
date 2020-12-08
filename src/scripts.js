9af9073eebbe717440435ad480a618fb353eb6a6
Boone Epstein <booneepstein@gmail.com>
2020-12-07 20:09:28 -0600

    Add html and js skeleton for welcome card

--- src/scripts.js
+++ src/scripts.js
@@ -1,2 +1,12 @@
 const userRepository = new UserRepository(userData);
 userRepository.createUsers();
+
+const user = userRepository.users[0]
+
+const welcomeMessage = document.querySelector('.welcome-message');
+const userInfo = document.querySelector('.user-info');
+const stepGoalCompare = document.querySelector('.step-goal-compare');
+
+welcomeMessage.innerText = `Welcome ${user.returnFirstName()}!`;
+userInfo.innerText = `${user.address} * ${user.email} * ${user.strideLength} * ${user.friends}`;
+stepGoalCompare.innerText = `Your step goal is ${user.dailyStepGoal} steps per day. The average goal is ${userRepository.averageStepGoal()} steps per day.`
