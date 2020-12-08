0877e6825125b7fa1d53e833328bdda260c7fa87
Boone Epstein <booneepstein@gmail.com>
2020-12-07 18:09:21 -0600

    Build out User class to pass tests

--- test/User-test.js
+++ test/User-test.js
@@ -15,67 +15,67 @@ describe('User', () => {
     'forrest@example.com', 3, 250, [12, 17, 2, 5]);
   });
 
-  it.skip('should have an id', () => {
+  it('should have an id', () => {
     expect(user1.id).to.equal(3);
   });
 
-  it.skip('should have a different id', () => {
+  it('should have a different id', () => {
     expect(user2.id).to.equal(4);
   });
 
-  it.skip('should have a name', () => {
+  it('should have a name', () => {
     expect(user1.name).to.equal('Briarhill Danton');
   });
 
-  it.skip('should have a different name', () => {
+  it('should have a different name', () => {
     expect(user2.name).to.equal('Forrest Cook');
   });
 
-  it.skip('should have an address', () => {
+  it('should have an address', () => {
     expect(user1.address).to.equal('1000 Good Boy Place, Treatsville NY 78552');
   });
 
-  it.skip('should have a different address', () => {
+  it('should have a different address', () => {
     expect(user2.address).to.equal('1234 Best Boy Ave, Las Vegas NV 80001');
   });
 
-  it.skip('should have an email', () => {
+  it('should have an email', () => {
     expect(user1.email).to.equal('danny@example.com');
   });
 
-  it.skip('should have a different email', () => {
+  it('should have a different email', () => {
     expect(user2.email).to.equal('forrest@example.com');
   });
 
-  it.skip('should have a stride length', () => {
+  it('should have a stride length', () => {
     expect(user1.strideLength).to.equal(2);
   });
 
-  it.skip('should have a diffent stride length', () => {
+  it('should have a diffent stride length', () => {
     expect(user2.strideLength).to.equal(3);
   });
 
-  it.skip('should have a daily step goal', () => {
+  it('should have a daily step goal', () => {
     expect(user1.dailyStepGoal).to.equal(100);
   });
 
-  it.skip('should have a different daily step goal', () => {
+  it('should have a different daily step goal', () => {
     expect(user2.dailyStepGoal).to.equal(250);
   });
 
-  it.skip('should have friends', () => {
+  it('should have friends', () => {
     expect(user1.friends).to.eql([19, 11, 42, 33]);
   });
 
-  it.skip('should have different friends', () => {
+  it('should have different friends', () => {
     expect(user2.friends).to.eql([12, 17, 2, 5]);
   });
 
-  it.skip('should return the first name', () => {
+  it('should return the first name', () => {
     expect(user1.returnFirstName()).to.equal('Briarhill');
   });
 
-  it.skip('should return a different first name', () => {
+  it('should return a different first name', () => {
     expect(user2.returnFirstName()).to.equal('Forrest');
   });
 });
