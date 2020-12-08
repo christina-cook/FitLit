1332b6dbb38f996c8ce672c35c1ef1ff4081414f
Christina Cook <cmc.cook7@gmail.com>
2020-12-08 14:44:35 -0700

    Add tests for Hydration and User

---
+++ test/Hydration-test.js
@@ -0,0 +1,29 @@
+const chai = require('chai');
+const expect = chai.expect;
+const UserRepository = require('../src/UserRepository');
+const User = require('../src/User');
+const Hydration = require('../src/Hydration');
+
+describe('Hydration', () => {
+
+  beforeEach(() => {
+
+  });
+
+  it.skip('Should have Hydration data', () => {
+
+  });
+
+  it.skip('Should get the average ounces consumed', () => {
+
+  });
+
+  it.skip('Should get the daily ounces consumed', () => {
+
+  });
+
+  it.skip('Should get the weekly ounces consumed', () => {
+
+  });
+
+});
