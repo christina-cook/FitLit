d5bce87d6c1351198d722ced8cecf0dd8c5b59ac
Christina Cook <cmc.cook7@gmail.com>
2020-12-09 18:30:59 -0700

    Add query selectors for buttons and dashboards

--- src/scripts.js
+++ src/scripts.js
@@ -7,6 +7,12 @@ const user = userRepository.users[0]
 const welcomeMessage = document.querySelector('.welcome-message');
 const userInfo = document.querySelector('.user-info');
 const stepGoalCompare = document.querySelector('.step-goal-compare');
+const homeDashboard = document.querySelector('.home-dashboard');
+const hydrationButton = document.querySelector('.hydration-button');
+const sleepButton = document.querySelector('.sleep-button');
+const activityButton = document.querySelector('.activityButton');
+const hydrationDashboard = document.querySelector('.hydration-dashboard');
+
 
 welcomeMessage.innerText = `Welcome ${user.returnFirstName()}!`;
 userInfo.innerText = `${user.address}\n${user.email}`;
