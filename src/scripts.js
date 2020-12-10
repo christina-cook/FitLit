45dd8a955939065ba12c365fb3b10c10b996bf51
Boone Epstein <booneepstein@gmail.com>
2020-12-09 21:03:07 -0600

    Create functions to update hydration dashboard with data

--- src/scripts.js
+++ src/scripts.js
@@ -4,6 +4,9 @@ userRepository.fetchAllHydration(hydrationData);
 
 const user = userRepository.users[0]
 
+const today = "2019/09/22"
+const week = ["16", "17", "18", "19", "20", "21", "22"]
+
 const welcomeMessage = document.querySelector('.welcome-message');
 const userInfo = document.querySelector('.user-info');
 const stepGoalCompare = document.querySelector('.step-goal-compare');
@@ -14,17 +17,28 @@ const hydrationIcon = document.querySelector('.hydration-icon');
 const sleepButton = document.querySelector('.sleep-button');
 const activityButton = document.querySelector('.activityButton');
 const hydrationDashboard = document.querySelector('.hydration-dashboard');
+const dailyHydration = document.querySelector('.daily-hydration');
+const weeklyHydration = document.querySelector('.weekly-hydration');
 
 
 welcomeMessage.innerText = `Welcome ${user.returnFirstName()}!`;
 userInfo.innerText = `${user.address}\n${user.email}`;
 stepGoalCompare.innerText = `Your step goal is ${user.dailyStepGoal} steps per day.\nThe average goal is ${userRepository.averageStepGoal()} steps per day.`
+dailyHydration.innerText = user.hydration.getDailyOz(today);
+weeklyHydration.innerText = `${user.hydration.getWeeklyOz(today)}\n${week}`;
 
 hydrationButton.addEventListener('click', toggleHydration);
+backButton.addEventListener('click', toggleBack);
 
 function toggleHydration() {
   homeDashboard.classList.toggle('hidden');
   hydrationDashboard.classList.toggle('hidden');
   backButton.classList.toggle('hidden');
   hydrationIcon.classList.toggle('hidden');
+}
+
+function toggleBack() {
+  if (!hydrationDashboard.classList.contains('hidden')) {
+    toggleHydration();
+  }
 }
