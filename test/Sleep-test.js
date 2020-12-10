7f7d6e1dc3ed6aef39da6c62a2d7cca21f486440
Boone Epstein <booneepstein@gmail.com>
2020-12-10 14:38:06 -0600

    Add skeleton of Hydration class and tests

---
+++ test/Sleep-test.js
@@ -0,0 +1,37 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Sleep = require('../src/Sleep');
+
+describe('Sleep', () => {
+  beforeEach(() => {
+
+  });
+
+  it.skip('Should have sleep data' () => {
+
+  });
+
+  it.skip('Should be able to get the average hours slept', () => {
+
+  });
+
+  it.skip('Should be able to get average sleep quality', () => {
+
+  });
+
+  it.skip('Should be able to get daily hours slept', () => {
+
+  });
+
+  it.skip('Should be able to get daily sleep quality', () => {
+
+  });
+
+  it.skip('Should get weekly average hours slept', () => {
+
+  });
+
+  it.skip('Should get weekly average sleep quality', () => {
+
+  });
+});
